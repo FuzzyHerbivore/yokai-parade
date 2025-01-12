45caae20822719adae6701698f10f88b607eaf44
Christian Schneider <fuzzyherbivore@icloud.com>
2025-01-12 13:54:20 +0100

    Added level manager to allow for loading multiple levels and refactored level loading and player spawning.

--- game/game.gd
+++ game/game.gd
@@ -1,5 +1,64 @@
 extends Node
 
 
+var _current_level_index = 0
+
+
 func _ready() -> void:
-	pass
+	var desired_level_index = 0
+	_load_level(desired_level_index)
+
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("reset_level"):
+		_load_level(_current_level_index)
+
+
+	if Input.is_action_just_pressed("load_next_level"):
+		var desired_level_index = _current_level_index + 1
+		if desired_level_index >= %LevelManager.number_of_levels:
+			print("Already at last level.")
+		else:
+			_load_level(desired_level_index)
+
+	if Input.is_action_just_pressed("load_previous_level"):
+		var desired_level_index = _current_level_index - 1
+		if desired_level_index < 0:
+			print("Already at first level.")
+		else:
+			_load_level(desired_level_index)
+
+
+func _load_level(desired_level_index):
+	var loaded_successfully = %LevelManager.load_level(desired_level_index)
+	if loaded_successfully:
+		_current_level_index = desired_level_index
+
+
+func _spawn_player(player_position):
+	if get_node_or_null("Player") != null:
+		var old_player = $Player
+		remove_child(old_player)
+		old_player.queue_free()
+
+	var player_scene = preload("res://player/player.tscn")
+
+	var player = player_scene.instantiate()
+	player.position = player_position
+	player.player_despawned.connect(_on_player_despawn)
+
+	var camera_node = get_tree().root.get_node_or_null("Game/Camera2D")
+
+	var remote_transform = RemoteTransform2D.new()
+	remote_transform.remote_path = camera_node.get_path()
+	player.add_child(remote_transform)
+
+	add_child(player)
+
+
+func _on_player_despawn():
+	_load_level(_current_level_index)
+
+
+func _on_level_manager_level_loaded(player_spawn_position):
+	_spawn_player(player_spawn_position)
--- levels/level.gd
+++ levels/level.gd
@@ -1,39 +1,11 @@
 extends Node2D
 
 
-func _ready():
-	_spawn_player()
-
-
-func _spawn_player():
-	var player_scene = preload("res://player/player.tscn")
-
-	var player = player_scene.instantiate()
-	player.transform = $PlayerSpawnPoint.transform
-	player.player_despawned.connect(_on_player_despawn)
-
-	var camera = Camera2D.new()
-	camera.position_smoothing_enabled = true
-	camera.position_smoothing_speed = 10.0
-
-	player.add_child(camera)
-
-	get_parent().add_child.call_deferred(player)
-
-
-func _unhandled_input(_event):
-	if Input.is_action_just_pressed("reset_level"):
-		_reload_level()
+var player_spawn_position:
+	get:
+		return $PlayerSpawnPoint.position
 
 
 func _on_despawn_area_body_entered(body):
 	if body.has_method("despawn"):
 		body.despawn()
-
-
-func _on_player_despawn():
-	_reload_level()
-
-
-func _reload_level():
-	get_tree().call_deferred("reload_current_scene")

89052ada5b4b31df0552b57d7d3763567444a0c9
TomGrzembke <grzembketom@gmail.com>
2025-02-04 11:56:10 +0000

    Added the option to turn of debug option by toggling the visibility of the node
    --HG--
    branch : player_attack

--- player/debug.gd
+++ player/debug.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var collision_shapE: CollisionShape2D = $"../CollisionShape2D"
 
 func _unhandled_input(event: InputEvent) -> void:
+	if !visible: return
+
 	if Input.is_action_just_pressed("DebugMode"):
 		var debug_mode = player.toggle_debug()
 		collision_shapE.disabled = debug_mode

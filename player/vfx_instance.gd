f446a9b1acd66c64cc3f6974b689bf9ecd1bdc7f
TomGrzembke <grzembketom@gmail.com>
2025-03-10 11:59:03 +0000

    Fixed ZZZ spawning and idling

--- player/vfx_instance.gd
+++ player/vfx_instance.gd
@@ -2,10 +2,16 @@ extends Node2D
 @onready var anim_sprite
 var rigid_body
 var freeze_physics
+var flip_parent
+var moved = false
+var clear_after_move
 
-func play(animation_name, emit_in_global = false, _freeze_physics = false, _z_index = null):
+
+func play(animation_name, emit_in_global = false, _freeze_physics = false, _z_index = null, _flip_parent = null, _clear_after_move = false):
 	rigid_body = $RigidBody2D
 	anim_sprite = $RigidBody2D/AnimatedSprite2D
+	flip_parent = _flip_parent
+	clear_after_move = _clear_after_move
 
 	if !has_anim(animation_name):
 		queue_free()
@@ -27,13 +33,35 @@ func to_root():
 
 
 func _physics_process(_delta):
+	flip()
+
+	clear_after_moving()
 	if rigid_body != null && rigid_body.freeze: return
 	if rigid_body != null && rigid_body.get_linear_velocity() == Vector2.ZERO:
 		rigid_body.freeze = true
 
 	if scale.y == 1 && rotation == 0 && (rigid_body == null || rigid_body.rotation == 0): return
+	if flip_parent != null: return
 
 	reset_rb()
+
+
+func flip():
+	if flip_parent == null: return
+
+	set_rotation_degrees(-180.0 if flip_parent.look_direction == -1.0 else 0.0)
+	scale.y = flip_parent.look_direction
+
+
+func clear_after_moving():
+	if !clear_after_move: return
+	if !moved: return
+
+	queue_free()
+
+
+func _unhandled_input(event):
+	moved = true
 
 
 func has_anim(animation_name):
